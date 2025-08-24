21e0ee57bcf296f8b785e2c6c61ce1c44184897b
nemdull <dlanx@outlook.jp>
2025-08-24 19:54:41 +0900

    Add: chapter3 calc

--- chapter3/ex0303.go
+++ chapter3/ex0303.go
@@ -7,5 +7,20 @@ func main() {
 	x := []int{}
 	fmt.Println(data == nil) //true
 	fmt.Println(x == nil)    //false
+	calc()
+}
+
+func calc() {
+	x := make([]string, 0, 5)
+	x = append(x, "a", "b", "c", "d")
+	y := x[:2]
+	z := x[2:]
+	fmt.Println(cap(x), cap(y), cap(z))
+	y = append(y, "i", "j", "k")
+	x = append(x, "y")
+	z = append(z, "y")
+	fmt.Println("x:", x)
+	fmt.Println("y:", y)
+	fmt.Println("z:", z)
 
 }
