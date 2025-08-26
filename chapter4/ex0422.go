c06e898d84b83802ac59a0eafc1ff2e18096509d
nemdull <dlanx@outlook.jp>
2025-08-26 23:20:44 +0900

    Add: ex0422

--- chapter4/ex0422.go
+++ chapter4/ex0422.go
@@ -20,4 +20,38 @@ func main() {
 			fmt.Printf("「%s」の文字数は%dで、とても長い!\n", word, rc)
 		}
 	}
+
+	switch_ex()
+	break_ex()
+}
+
+func switch_ex() {
+	words := []string{"hi", "salutations", "hello"}
+	for _, word := range words {
+		switch wordLen := len(word); {
+		case wordLen < 5:
+			fmt.Println(word, "は短い単語です")
+		case wordLen > 10:
+			fmt.Println(word, "は長すぎる単語です")
+		default:
+			fmt.Println(word, "はちょうど良い長さです")
+		}
+	}
+}
+
+func break_ex() {
+loop:
+	for i := 0; i < 10; i++ {
+		switch {
+		case i%2 == 0:
+			fmt.Println(i, ":even")
+		case i%3 == 0:
+			fmt.Println(i, ":mod 3 but not mod 2")
+		case i%7 == 0:
+			fmt.Println(i, ":end of loop")
+			break loop //label
+		default:
+			fmt.Println(i, ":boaring number")
+		}
+	}
 }
